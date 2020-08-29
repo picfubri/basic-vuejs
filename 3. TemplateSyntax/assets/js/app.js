b7478d67007db151f26a47208572675ebcb4bbbd
BangIAS <iqbal.a.sofyan@gmail.com>
2020-08-29 11:01:22 +0700

    js

--- 3. TemplateSyntax/assets/js/app.js
+++ 3. TemplateSyntax/assets/js/app.js
@@ -1,6 +1,23 @@
-var app = new Vue({ 
+/* Interpolation */
+var app = new Vue({ // TEXTS
     el:'#app-1', 
     data:{ 
+        text: 'Hai!, this is Text',
+        text1: 'Try It'
+    }
+})
+var app = new Vue({ // Raw HTML
+    el:'#app-2', 
+    data:{ 
+        rawHTML: 'Uusing Text Mustache and RawHTML',
+
+    }
+})
+var app = new Vue({ // Attribute
+    el:'#app-3', 
+    data:{ 
+        isActive: true,
+        hasError: false
 
     }
 })

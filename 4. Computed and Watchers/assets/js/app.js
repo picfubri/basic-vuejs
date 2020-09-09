ce04b3ae425899547d96beeb4d3bcf27952e8edd
picfubri <picfubri@gmail.com>
2020-09-09 21:38:03 +0700

    same

--- 4. Computed and Watchers/assets/js/app.js
+++ 4. Computed and Watchers/assets/js/app.js
@@ -13,6 +13,9 @@ var app = new Vue({ // Light Logic
     computed:{
         reversedMessage: function() {
             return this.message.split('').reverse().join('')
+        },
+        now:function() {
+            return Date.now()
         }
     },
     methods: {
@@ -22,9 +25,34 @@ var app = new Vue({ // Light Logic
     }
 })
 
-var app = new Vue({ // Computed Cache vs Metode
-    el:'#computedCache',
-    
+/* Computed vs Watch Properties */
+var app = new Vue({ // Watch
+    el:'#computedWatch', 
+    data:{ 
+       firstName: 'Iqbal Ali',
+       lastName: 'Sofyan',
+       fullName: 'Iqbal Ali Sofyan'
+    },
+    watch:{
+        firstName: function(val) {
+            this.fullName = val + ' ' + this.lastName
+        },
+        lastName: function(val) {
+            this.fullName = this.firstName + ' ' + val
+        }
+    }
 })
 
+var app = new Vue({ // Computed
+    el:'#computedWatch1', 
+    data:{ 
+       firstName: 'Iqbal Ali',
+       lastName: 'Sofyan'
+    },
+    computed:{
+       fullName: function() {
+            return this.firstName + ' ' + this.lastName
+       }
+    }
+})
 /* To Be Continued */

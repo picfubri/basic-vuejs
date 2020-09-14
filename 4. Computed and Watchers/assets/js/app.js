a304b1e8cea061b3405d4bb51de62f7f2f459da7
picfubri <picfubri@gmail.com>
2020-09-14 22:24:20 +0700

    update watch-example

--- 4. Computed and Watchers/assets/js/app.js
+++ 4. Computed and Watchers/assets/js/app.js
@@ -1,3 +1,5 @@
+
+
 /* Computed */
 var app = new Vue({ // Heavy Logic
     el:'#heavyLogic', 
@@ -51,18 +53,52 @@ var app = new Vue({ // Computed
     },
     computed:{
         fullName: {
-            get: function() {
+            get: function() { //Computed Getter
                 return this.fullName + ' ' + this.lastName
             },
-            set: function() {
+            set: function() { //Computed Setter
                 var names = newValue.split(' ')
                 this.firstName = name[0]
                 this.lastName = name[name.length - 1]
             }
         }
-       /*fullName: function() {
+       /*fullName: function() { //OriginalComputed
             return this.firstName + ' ' + this.lastName
        }*/
     }
 })
-/* To Be Continued */
+
+
+var watchExamleVM = new Vue({
+    el: '#watch-example',
+    data:{
+        question: '',
+        answer: "I can't give you an answer until you ask me!",
+    },
+    watch:{
+        question:function (newQuestion, oldQuestion) {
+            this.answer = 'waiting for you to stop typing...'
+            this.debouncedGetAnswer()
+        }
+    },
+    created: function () {
+        this.debouncedGetAnswer = _.debounce(this.debouncedGetAnswer, 500)
+    },
+    methods:{
+        getAnswer: function () {
+            if(this.question.indexOf('?')==-1){
+                this.answer = 'Questions must have a question mark'
+                return
+            }
+            this.answer = 'Think...'
+            var vm = this
+            axios.get('https//yesno.wtf/api')
+                .then(function (response) {
+                    vm.answer = _.capitalize(response.data.answer)
+                })
+                .catch(function (error) {
+                    vm.answer = 'Error! unable to process API. ' + error
+                })
+        }
+    }
+})
